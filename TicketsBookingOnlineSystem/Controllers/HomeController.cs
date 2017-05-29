6cc599629d90a09ad957f63643a0aa472386f595
pigeon <kacperx95@gmail.com>
2017-05-29 17:20:24 +0200

    connectingDB

--- TicketsBookingOnlineSystem/Controllers/HomeController.cs
+++ TicketsBookingOnlineSystem/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TicketsBookingOnlineSystem.Context;
 
 namespace TicketsBookingOnlineSystem.Controllers
 {
     public class HomeController : Controller
     {
+        CinemaDbContext db = new CinemaDbContext();
+
         public ActionResult Index()
         {
             return View();
