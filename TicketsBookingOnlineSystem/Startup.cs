81adb0ae1cea03c1aebc2961e1773f5a04d7b308
pigeon <kacperx95@gmail.com>
2017-08-18 13:31:18 +0200

    Usuniety startup

--- TicketsBookingOnlineSystem/Startup.cs
+++ TicketsBookingOnlineSystem/Startup.cs
@@ -1,14 +1,14 @@
-﻿using Microsoft.Owin;
-using Owin;
+﻿//using Microsoft.Owin;
+//using Owin;
 
-[assembly: OwinStartupAttribute(typeof(TicketsBookingOnlineSystem.Startup))]
-namespace TicketsBookingOnlineSystem
-{
-    public partial class Startup
-    {
-        public void Configuration(IAppBuilder app)
-        {
-            ConfigureAuth(app);
-        }
-    }
-}
+//[assembly: OwinStartupAttribute(typeof(TicketsBookingOnlineSystem.Startup))]
+//namespace TicketsBookingOnlineSystem
+//{
+//    public partial class Startup
+//    {
+//        public void Configuration(IAppBuilder app)
+//        {
+//            ConfigureAuth(app);
+//        }
+//    }
+//}
